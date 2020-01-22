06f11f4215038ff9409a23c3bb32ac92c4a336c8
AndBreis <49394086+AndBreis@users.noreply.github.com>
2020-01-22 13:06:20 +0100

    Add files via upload

--- foo.c
+++ foo.c
@@ -1,27 +1,32 @@
 #include <pthread.h>
 #include <stdio.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 int i = 0;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
-    for (int x = 0; x < 1000000 ; x++){
-      i++
+    for (int x = 0 ; x < 1000000 ; x++) {
+      i++;
     }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
-    for (int x = 0; x < 1000000 ; x++){
-      i--
+    for (int k = 0; k < 1000000 ; k++){
+      i--;
     }
     return NULL;
 }
 
 
 int main(){
-    pthread_t incrementingThreadFunction
-    pthread_t decrementingThreadFunction
+    pthread_t incrementingThread;
+    pthread_t decrementingThread;
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
@@ -30,5 +35,6 @@ int main(){
     pthread_join(decrementingThread, NULL);
 
     printf("The magic number is: %d\n", i);
+    sleep(5);
     return 0;
 }
